0923f24acfe24f6a535191bdcb59d71cf4570d1d
willywil <jonwilhelmjr@hotmail.com>
2023-03-07 20:13:12 -0600

    Update Chatgpt.py
    trying to add attack and collect resources

--- Chatgpt.py
+++ Chatgpt.py
@@ -49,13 +49,27 @@ def print_field():
         
 def print_resources():
     print(RESOURCE_LABEL + "Human: " + str(human_resources) + "   Computer: " + str(computer_resources))
-        
+
+def collect_resources(resource_truck):
+    if resource_truck == 1:
+        human_resources += 1
+    elif resource_truck == 2:
+        computer_resources += 1
+
 def get_random_empty_location():
     while True:
         i = random.randint(0, FIELD_SIZE-1)
         j = random.randint(0, FIELD_SIZE-1)
         if field[i][j] == EMPTY:
             return i, j
+
+def get_attack_unit(field, position(i,j), player):
+    for i in range(FIELD_SIZE):
+        for j in range(FIELD_SIZE):
+            if (field[i][j] == HUMAN_UNIT or field[i][j] == COMPUTER_UNIT) and abs(i - position[0]) <= 1 and abs(j - position[1]) <= 1 and (i, j) != position and field[i][j] != player:
+                return (i, j)
+    return None
+
 
 # Main game loop
 while True:
@@ -97,3 +111,5 @@ while True:
                 i, j = get_random_empty_location()
                 field[i][j] = HUMAN_UNIT
                 human_resources -= 5
+                get_attack_unit(field(i,j), position(i,j), player(HUMAN_PLAYER,COMPUTER_ENEMY))
+                print(get_attack_unit())
